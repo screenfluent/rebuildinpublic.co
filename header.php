646f0cd8cafeb2368cb96f891ca88ec56bde49b6
Szymon  Raczka <github.com.screenfluentpro@accs.mindfulmo.de>
2024-03-24 22:28:29 +0100

    Update CSS file path in header.php

--- header.php
+++ header.php
@@ -3,6 +3,6 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="build.css" rel="stylesheet">
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/build.css" rel="stylesheet">
 </head>
 <body <?php body_class(); ?>>
