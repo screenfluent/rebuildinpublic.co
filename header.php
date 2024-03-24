0fea2a46ef758e2e28b3f4068a0a849fe663da59
Szymon  Raczka <github.com.screenfluentpro@accs.mindfulmo.de>
2024-03-24 21:50:10 +0100

    Add build.css stylesheet to header.php and conditionally enable cssnano in postcss.config.js

--- header.php
+++ header.php
@@ -3,6 +3,6 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+    <link href="build.css" rel="stylesheet">
 </head>
 <body <?php body_class(); ?>>
