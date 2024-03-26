ba47573b94f1693c664217826275c6fb94136a4e
Szymon  Raczka <github.com.screenfluentpro@accs.mindfulmo.de>
2024-03-26 18:22:15 +0100

    Refactor content.php and index.php, add post meta and post content styles

--- content.php
+++ content.php
@@ -1,4 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <h2 class="font-bold text-lg"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <p><?php the_excerpt(); ?></p>
+    <header>
+        <h2 class="font-bold text-lg"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <div class="post-meta">
+            <span>Posted on <?php the_date(); ?></span>
+            <span>By <?php the_author(); ?></span>
+        </div>
+    </header>
+    <div class="post-content">
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>">Read More</a>
+    </div>
 </article>
--- index.php
+++ index.php
@@ -6,14 +6,20 @@
         <img class="w-12 h-12 rounded-full mb-4" src="https://framerusercontent.com/images/B0daymYlUsfgXpHo9PjmDZJv1sA.jpg" alt="Blog Logo">
         <h1 class="font-bold text-sm mb-8"><?php echo get_bloginfo('name'); ?></h1>
         <ul class="flex space-x-4 text-sm lg:text-15 -mb-1">
-            <li>
-                <?php if (is_front_page()): ?>
-                    My Story
-                <?php else: ?>
-                    <a class="hover:text-gray-700" href="<?php echo esc_url(home_url('/')); ?>">My Story</a>
-                <?php endif; ?>
-            </li>
-            <li><a class="text-gray-500 hover:text-gray-700" href="<?php echo esc_url(home_url('/blog')); ?>">Blog</a></li>
+    <li class="<?php echo (is_front_page() ? 'current-page-item' : ''); ?>">
+        <?php if (is_front_page()): ?>
+            My Story
+        <?php else: ?>
+            <a class="text-gray-500 hover:text-gray-700" href="<?php echo esc_url(home_url('/')); ?>">My Story</a>
+        <?php endif; ?>
+    </li>
+    <li class="<?php echo (is_home() ? 'current-page-item' : ''); ?>">
+        <?php if (is_home()): ?>
+            Blog
+        <?php else: ?>
+            <a class="text-gray-500 hover:text-gray-700" href="<?php echo esc_url(home_url('/blog')); ?>">Blog</a>
+        <?php endif; ?>
+    </li>
             <li><a class="text-gray-500 hover:text-gray-700" href="<?php echo esc_url(home_url('/projects')); ?>">Projects</a></li>
             <li><a class="text-gray-500 hover:text-gray-700" href="<?php echo esc_url(home_url('/morning-walks')); ?>">Morning Walks</a></li>
         </ul>
